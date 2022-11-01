6368b3851e3ba24bfe20d69f25b7056c64ffb3be
proxyhost <115903085+proxyhost@users.noreply.github.com>
2022-11-01 18:02:04 -0400

    Delete about blank proxy.js

--- about blank proxy.js
+++
@@ -1,6 +0,0 @@
-var win = window.open(); 
-var url = 'https://ultraviolet-node.galaxybender.repl.co/'; 
-var iframe = win.document.createElement('iframe'); 
-iframe.style="position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;"; 
-iframe.src = url; 
-win.document.body.appendChild(iframe);
